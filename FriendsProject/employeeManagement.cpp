a11879987e135a6afbc13a7ee32844d7e97efe98
김휘진 <zedion@gmail.com>
2022-02-15 21:21:38 +0900

    [Feature] EmployeeManagement class
    - EmployeeManagement class 구현
     : dataManager 호출 부분 임의로 코드 작성 및 주석 처리 함
     : @공종욱 @신승준 확인 부탁드립니다.
    - main 함수 구현

--- FriendsProject/employeeManagement.cpp
+++ FriendsProject/employeeManagement.cpp
@@ -2,11 +2,27 @@
 
 #include <iostream>
 #include "employeeInfo.h"
+#include "employeeManagement.h"
 #include "CommandParser.h"
 
 using namespace std;
 
 int main(void) {
+	EmployeeManagement* em = new EmployeeManagement();
+	int data_size = em->loadData("../FriendsProject/input_20_20.txt");
+	if (data_size < 0) return -1;
+
+	int result = 0;
+	for (int i = 0; i < data_size; i++) {
+		result = em->parseCommand(i);
+		if (result < 0) continue;
+
+		result = em->runCommand(i);
+		if (result < 0) continue;
+
+		result = em->printResult(i);
+		if (result < 0) continue;
+	}
 
 	return 0;
 }
