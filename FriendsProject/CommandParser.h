7999ec4d58348f4330649cc9afd10f63e96403ac
김휘진 <zedion@gmail.com>
2022-02-15 21:26:02 +0900

    [Feature] CommandParser class 구현
    - 각 method에 대해 구현 완료
    - 관련 된 TDD Pass 확인
    - option에 대한 처리 코드 누락, 추가 필요
     -> 관련 된 testcase 우선 추가 후, 코드 구현 예정

--- FriendsProject/CommandParser.h
+++ FriendsProject/CommandParser.h
@@ -24,22 +24,112 @@ struct KeyInfo {
 class CommandParser {
 public:
 	CommandType parseData(string readLine) {
-		return CommandType::INVALID;
+		parseLine = split(readLine, ',');
+
+		if (parseLine[0] == "ADD") commandType = CommandType::ADD;
+		else if (parseLine[0] == "MOD") commandType = CommandType::MOD;
+		else if (parseLine[0] == "SCH") commandType = CommandType::SCH;
+		else if (parseLine[0] == "DEL") commandType = CommandType::DEL;
+		else commandType = CommandType::INVALID;
+
+		return commandType;
 	}
 
 	int parseAddCommand(EmployeeInfo* addInfo) {
+		if (commandType != CommandType::ADD)
+			return -1;
+
+		addInfo->employeeNum = atoi(parseLine[4].c_str());
+
+		vector<string> name_split = split(parseLine[5], ' ');
+		if (2 != name_split.size())
+			return -1;
+
+		addInfo->givenName = name_split[0];
+		addInfo->familyName = name_split[1];
+
+		addInfo->cl = (CareerLevel)(atoi(parseLine[6].substr(2, 1).c_str()) - 1);
+		if ((addInfo->cl < CareerLevel::CL1) || (addInfo->cl > CareerLevel::CL4))
+			return -1;
+
+		vector<string> phone_split = split(parseLine[7], '-');
+		if (3 != phone_split.size())
+			return -1;
+
+		addInfo->phoneNumMid = atoi(phone_split[1].c_str());
+		addInfo->phoneNumEnd = atoi(phone_split[2].c_str());
+
+		// TODO: add birth invalid condition
+		addInfo->birthYear = atoi(parseLine[8].substr(0, 4).c_str());
+		addInfo->birthMonth = atoi(parseLine[8].substr(4, 2).c_str());
+		addInfo->birthDay = atoi(parseLine[8].substr(6, 2).c_str());
+
+		string lastStr = parseLine[9].substr(0, (parseLine[9].length() - 1));
+		if (lastStr == "ADV") addInfo->certi = CERTI::ADV;
+		else if (lastStr == "PRO") addInfo->certi = CERTI::PRO;
+		else if (lastStr == "EX") addInfo->certi = CERTI::EX;
+		else return -1;
+
 		return 0;
 	}
 
 	int parseModifyCommand(KeyInfo* keyInfo) {
+		if (commandType != CommandType::MOD)
+			return -1;
+
+		keyInfo->searchKey = parseLine[4];
+		keyInfo->searchKeyword = parseLine[5];
+		keyInfo->modifyKey = parseLine[6];
+
+		string lastStr = parseLine[7].substr(0, (parseLine[7].length() - 1));
+		keyInfo->modifyKeyword = lastStr;
+
 		return 0;
 	}
 
 	int parseDeleteCommand(KeyInfo* keyInfo) {
+		if (commandType != CommandType::DEL)
+			return -1;
+
+		keyInfo->searchKey = parseLine[4];
+
+		string lastStr = parseLine[5].substr(0, (parseLine[5].length() - 1));
+		keyInfo->searchKeyword = lastStr;
+
 		return 0;
 	}
 
 	int parseSearchCommand(KeyInfo* keyInfo) {
+		if (commandType != CommandType::SCH)
+			return -1;
+
+		keyInfo->searchKey = parseLine[4];
+
+		string lastStr = parseLine[5].substr(0, (parseLine[5].length() - 1));
+		keyInfo->searchKeyword = lastStr;
+
 		return 0;
 	}
+
+private:
+	vector<string> split(string text, char div) {
+		vector<string> result;
+		text.push_back(div);
+		int a = 0, b = 0;
+		while (1) {
+			b = (int)text.find(div, a);
+			if (b == -1) break;
+
+			string temp = text.substr(a, b - a);
+			result.push_back(temp);
+
+			a = b + 1;
+		}
+
+		return result;
+	}
+
+	CommandType commandType;
+	FILE* fin;
+	vector<string> parseLine;
 };
