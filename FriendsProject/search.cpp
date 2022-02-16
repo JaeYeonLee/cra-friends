dc9a04b49b3177c5290b5ae46aa516bce1f93a7d
이재연 <jyegglee@gmail.com>
2022-02-17 07:30:06 +0900

    [Feaure] dataManager.schEmployee 구현

--- FriendsProject/search.cpp
+++ FriendsProject/search.cpp
@@ -1,21 +1,21 @@
 #include "search.h"
 
-const list<EmployeeInfo*>* SearchEngine::search(string key, unordered_map<string, list<EmployeeInfo*>>& hashTable) {
+list<EmployeeInfo*>* SearchEngine::search(string key, unordered_map<string, list<EmployeeInfo*>>& hashTable) {
 	auto result = hashTable.find(key);
 	if (result == hashTable.end()) return nullptr;
 	return &result->second;
 }
-const list<EmployeeInfo*>* SearchEngine::search(int key, unordered_map<int, list<EmployeeInfo*>>& hashTable) {
+list<EmployeeInfo*>* SearchEngine::search(int key, unordered_map<int, list<EmployeeInfo*>>& hashTable) {
 	auto result = hashTable.find(key);
 	if (result == hashTable.end()) return nullptr;
 	return &result->second;
 }
-const list<EmployeeInfo*>* SearchEngine::search(CERTI key, unordered_map<CERTI, list<EmployeeInfo*>>& hashTable) {
+list<EmployeeInfo*>* SearchEngine::search(CERTI key, unordered_map<CERTI, list<EmployeeInfo*>>& hashTable) {
 	auto result = hashTable.find(key);
 	if (result == hashTable.end()) return nullptr;
 	return &result->second;
 }
-const list<EmployeeInfo*>* SearchEngine::search(CareerLevel key, unordered_map<CareerLevel, list<EmployeeInfo*>>& hashTable) {
+list<EmployeeInfo*>* SearchEngine::search(CareerLevel key, unordered_map<CareerLevel, list<EmployeeInfo*>>& hashTable) {
 	auto result = hashTable.find(key);
 	if (result == hashTable.end()) return nullptr;
 	return &result->second;
--- UnitTest/dataManager_cmd_test.cpp
+++ UnitTest/dataManager_cmd_test.cpp
@@ -1,5 +1,4 @@
 #include "pch.h"
-//#include "../FriendsProject/employeeInfo.h"
 #include "../FriendsProject/dataManager.h"
 
 void input_test_data(DataManager* data_manager) { // test iuput data for cmd run
@@ -22,105 +21,105 @@ TEST(dataManagerTest, DelEmployeeTest_Num) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(1, data_manager->schEmployee({ "employeeNum", "00000000" }));
-	EXPECT_EQ(1, data_manager->schEmployee({ "employeeNum", "00000001" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "employeeNum", "00000000" }));
+	EXPECT_EQ(1, data_manager->delEmployee({ "employeeNum", "00000000" }));
+	EXPECT_EQ(1, data_manager->delEmployee({ "employeeNum", "00000001" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "employeeNum", "00000000" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_Name_Full) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "name", "KILDONG HONG" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "name", "DONGKIL KIM" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "name", "KILDONG HONG" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "name", "KILDONG HONG" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "name", "DONGKIL KIM" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "name", "KILDONG HONG" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_Name_Given) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "givenName", "KILDONG" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "givenName", "DONGKIL" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "givenName", "KILDONG" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "givenName", "KILDONG" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "givenName", "DONGKIL" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "givenName", "KILDONG" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_Name_Family) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "familyName", "HONG" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "familyName", "KIM" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "familyName", "HONG" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "familyName", "HONG" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "familyName", "KIM" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "familyName", "HONG" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_Cl) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "cl", "1" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "cl", "2" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "cl", "1" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "cl", "1" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "cl", "2" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "cl", "1" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_PhoneNum) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "phoneNum", "010-1234-5678" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "phoneNum", "010-8765-4321" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "phoneNum", "010-1234-5678" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "phoneNum", "010-1234-5678" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "phoneNum", "010-8765-4321" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "phoneNum", "010-1234-5678" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_PhoneNumMid) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "phoneNumMid", "1234" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "phoneNumMid", "8765" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "phoneNumMid", "1234" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "phoneNumMid", "1234" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "phoneNumMid", "8765" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "phoneNumMid", "1234" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_PhoneNumEnd) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "phoneNumEnd", "5678" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "phoneNumEnd", "4321" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "phoneNumEnd", "5678" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "phoneNumEnd", "5678" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "phoneNumEnd", "4321" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "phoneNumEnd", "5678" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_Birth) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "birthday", "19991231" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "birthday", "20000101" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "birthday", "19991231" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "birthday", "19991231" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "birthday", "20000101" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "birthday", "19991231" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_BirthYear) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "birthdayYear", "1999" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "birthdayYear", "2000" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "birthdayYear", "1999" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "birthdayYear", "1999" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "birthdayYear", "2000" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "birthdayYear", "1999" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_BirthMonth) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "birthdayMonth", "12" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "birthdayMonth", "01" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "birthdayMonth", "12" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "birthdayMonth", "12" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "birthdayMonth", "01" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "birthdayMonth", "12" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_BirthDay) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "birthdayDay", "31" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "birthdayDay", "01" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "birthdayDay", "31" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "birthdayDay", "31" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "birthdayDay", "01" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "birthdayDay", "31" }));
 }
 TEST(dataManagerTest, DelEmployeeTest_Certi) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "certi", "ADV" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "certi", "PRO" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "certi", "ADV" }));
+	EXPECT_EQ(3, data_manager->delEmployee({ "certi", "ADV" }));
+	EXPECT_EQ(2, data_manager->delEmployee({ "certi", "PRO" }));
+	EXPECT_EQ(0, data_manager->delEmployee({ "certi", "ADV" }));
 }
 
 
@@ -128,49 +127,49 @@ TEST(dataManagerTest, SchEmployeeTest_Num) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(1, data_manager->schEmployee({ "employeeNum", "00000000" }));
-	EXPECT_EQ(1, data_manager->schEmployee({ "employeeNum", "00000001" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "employeeNum", "00000006" }));
+	EXPECT_EQ(1, data_manager->schEmployee({ "employeeNum", "00000000" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(1, data_manager->schEmployee({ "employeeNum", "00000001" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(0, data_manager->schEmployee({ "employeeNum", "00000006" }, { Option::NONE, Option::NONE }));
 }
 TEST(dataManagerTest, SchEmployeeTest_Name_Full) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "name", "KILDONG HONG" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "name", "DONGKIL KIM" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "name", "AAAAAAA AAAA" }));
+	EXPECT_EQ(3, data_manager->schEmployee({ "name", "KILDONG HONG" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(2, data_manager->schEmployee({ "name", "DONGKIL KIM" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(0, data_manager->schEmployee({ "name", "AAAAAAA AAAA" }, { Option::NONE, Option::NONE }));
 }
 TEST(dataManagerTest, SchEmployeeTest_Cl) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "cl", "1" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "cl", "2" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "cl", "3" }));
+	EXPECT_EQ(3, data_manager->schEmployee({ "cl", "1" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(2, data_manager->schEmployee({ "cl", "2" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(0, data_manager->schEmployee({ "cl", "3" }, { Option::NONE, Option::NONE }));
 }
 TEST(dataManagerTest, SchEmployeeTest_Phone) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "phoneNum", "010-1234-5678" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "phoneNum", "010-8765-4321" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "phoneNum", "010-0000-0000" }));
+	EXPECT_EQ(3, data_manager->schEmployee({ "phoneNum", "010-1234-5678" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(2, data_manager->schEmployee({ "phoneNum", "010-8765-4321" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(0, data_manager->schEmployee({ "phoneNum", "010-0000-0000" }, { Option::NONE, Option::NONE }));
 }
 TEST(dataManagerTest, SchEmployeeTest_Birth) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "birthday", "19991231" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "birthday", "20000101" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "birthday", "20010615" }));
+	EXPECT_EQ(3, data_manager->schEmployee({ "birthday", "19991231" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(2, data_manager->schEmployee({ "birthday", "20000101" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(0, data_manager->schEmployee({ "birthday", "20010615" }, { Option::NONE, Option::NONE }));
 }
 TEST(dataManagerTest, SchEmployeeTest_Certi) {
 	DataManager* data_manager = new DataManager();
 	input_test_data(data_manager);
 
-	EXPECT_EQ(3, data_manager->schEmployee({ "certi", "ADV" }));
-	EXPECT_EQ(2, data_manager->schEmployee({ "certi", "PRO" }));
-	EXPECT_EQ(0, data_manager->schEmployee({ "certi", "EX" }));
+	EXPECT_EQ(3, data_manager->schEmployee({ "certi", "ADV" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(2, data_manager->schEmployee({ "certi", "PRO" }, { Option::NONE, Option::PRINT }));
+	EXPECT_EQ(0, data_manager->schEmployee({ "certi", "EX" }, { Option::NONE, Option::NONE }));
 }
 
 
