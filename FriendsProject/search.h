f7f24feb5b53ae3e545132779092be0a0b96b40e
이재연 <jyegglee@gmail.com>
2022-02-16 20:02:38 +0900

    [bugFix] test code 제거

--- FriendsProject/search.h
+++ FriendsProject/search.h
@@ -6,7 +6,6 @@ using namespace std;
 
 class SearchEngine {
 public:
-	int add(int a, int b) { return a + b; }
 	const list<EmployeeInfo*>* search(string key, unordered_map<string, list<EmployeeInfo*>>& hashTable);
 	const list<EmployeeInfo*>* search(int key, unordered_map<int, list<EmployeeInfo*>>& hashTable);
 	const list<EmployeeInfo*>* search(CERTI key, unordered_map<CERTI, list<EmployeeInfo*>>& hashTable);
